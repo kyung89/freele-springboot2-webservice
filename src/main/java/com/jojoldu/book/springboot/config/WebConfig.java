42c4fba48e5d06e86826754df8627e3936028ffa
kyung <bamtori1029@gmail.com>
2024-11-05 18:38:24 +0900

    500 에러 관련 수정

--- src/main/java/com/jojoldu/book/springboot/config/WebConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.jojoldu.book.springboot.config.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,5 +17,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(loginUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                //.allowCredentials(true) //주석처리함
+                .allowedOriginPatterns("*") //추가한 부분
+                .exposedHeaders("*");
     }
 }
